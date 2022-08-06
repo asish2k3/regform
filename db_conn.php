0ac060b83a675ea1d70153c1ad5facced8b12492
Asish <83742617+asishsingamsetty@users.noreply.github.com>
2022-08-06 22:55:05 +0530

    Add files via upload

---
+++ db_conn.php
@@ -0,0 +1,14 @@
+<?php  
+
+$sname = "localhost:3306";
+$uname = "root";
+$password = "";
+
+$db_name = "test";
+
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+if (!$conn) {
+	echo "Connection failed!";
+	exit();
+}
